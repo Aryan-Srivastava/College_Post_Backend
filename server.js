0153a8a4450b7ba29a4267ad8d8a118338b1ca35
Aryan <aryanyash999@gmail.com>
2023-01-01 19:42:43 +0530

    updated server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoute = require('./routes/auth_routes');
-const userRoute = require('./routes/users_routes');
-const postRoute = require('./routes/posts_routes');
-const categoryRoute = require('./routes/categories_routes');
-const multer = require('multer');
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth_routes");
+const userRoute = require("./routes/users_routes");
+const postRoute = require("./routes/posts_routes");
+const categoryRoute = require("./routes/categories_routes");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -14,26 +15,29 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 // Connect MongoDB
 mongoose
-    .connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.error('Could not connect to MongoDB...'))
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to MongoDB..."))
+	.catch((err) => console.error(err));
 
 // Upload image
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, req.body.name);
-    },
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
 });
 const upload = multer({ storage: storage });
-app.post('/api/upload', upload.single('file'), (req, res) => {
-    res.status(200).json('File has been uploaded');
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	res.status(200).json("File has been uploaded");
 });
 
 // Routes
@@ -41,3 +45,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+
+// Start server
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
